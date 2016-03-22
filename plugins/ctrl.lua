11eef0c74bf64740edb27fbf1d552b00efc8b6ce
gmorishere <aryan.shahin@icloud.com>
2016-03-23 02:10:02 +0430

    Update ctrl.lua

--- plugins/ctrl.lua
+++ plugins/ctrl.lua
@@ -41,7 +41,7 @@ local function pre_process(msg)
 	
 	-- If sender is sudo then re-enable the channel
 	if is_sudo(msg) then
-	  if msg.text == "#bot on" then
+	  if msg.text == "bot on" then
 	    enable_channel(receiver, msg.to.id)
 	  end
 	end
@@ -71,8 +71,13 @@ end
 
 return {
 	patterns = {
-		"^#bot? (on)",
-		"^#bot? (off)" }, 
+		"^[#!/]bot? (on)",
+		"^[!/#]bot? (off)",
+		"^bot? (on)",
+		"^bot? (off)", 		
+		"^Bot? (on)",
+		"^Bot? (off)"		
+		}, 
 	run = run,
 	pre_process = pre_process
 }
